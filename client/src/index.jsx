3743373f70e016d3b92fa3eb7e2e68257e3f3fa9
Young Kim <youngkim113@gmail.com>
2021-10-06 17:29:20 -0700

    Rendered App into DOM by importing App, React, and ReactDOM to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,6 @@
 // TODO: Render the `App` component to the DOM
+import App from './components/App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('app'));
